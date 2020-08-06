08d0a979cb6b6ca9170bc1acff7e3361764d9052
IEWbgfnYDwHRoRRSKtkdyMDUzgdwuBYgDKtDJWd <34784672+IEWbgfnYDwHRoRRSKtkdyMDUzgdwuBYgDKtDJWd@users.noreply.github.com>
2020-08-05 20:02:14 -0400

    Create moviebarcode.sh

---
+++ moviebarcode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function vidinfo () {
+	INPUT="${FILE}"
+	width=$(ffprobe -select_streams v -show_streams $INPUT 2>/dev/null | grep coded_width | sed -e 's/coded_width=//')
+	height=$(ffprobe -select_streams v -show_streams $INPUT 2>/dev/null | grep coded_height | sed -e 's/coded_height=//')
+
+	echo ------------------------------- 
+	echo imginfo: $INPUT 
+	echo  
+	echo width: "$width"
+	echo height: "$height"
+	echo ------------------------------- 
+}
+
+FILE=$1
+
+
+		vidinfo;
+		python3 barcode.py -video "$FILE" -width $width -height $width
