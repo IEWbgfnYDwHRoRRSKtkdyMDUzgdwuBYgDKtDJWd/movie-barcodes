6038b8a37d136f84046dd711251c89c92a979b55
IEWbgfnYDwHRoRRSKtkdyMDUzgdwuBYgDKtDJWd <34784672+IEWbgfnYDwHRoRRSKtkdyMDUzgdwuBYgDKtDJWd@users.noreply.github.com>
2020-08-06 00:25:38 -0400

    Update moviebarcode.sh

--- moviebarcode.sh
+++ moviebarcode.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#!bash moviebarcode.sh video6.mp4 6 batchno
+#!bash moviebarcode.sh video6.mp4 6 batchyes
 
 function vidinfo () {
 	INPUT="${FILE}"
@@ -13,9 +15,42 @@ function vidinfo () {
 	echo ------------------------------- 
 }
 
-FILE=$1
-widthfactor=$2
-
+function batchno () {
 
 		vidinfo;
 		python3 barcode.py -video "$FILE" -width $((width*widthfactor)) -height $height
+		
+}
+
+function batchyes () {
+
+		mkdir out
+		while read line; do
+		
+			mkdir temp
+			cd temp
+			FILE=$(youtube-dl -s --get-filename -f 'bestvideo[height<=1080]' $line -o '%(id)s.%(ext)s')
+			youtube-dl -f 'bestvideo[height<=1080]' $line -o '%(id)s.%(ext)s'
+			vidinfo;
+			mv $FILE ../$FILE
+			cd ../
+			python3 barcode.py -video "$FILE" -width $((width*widthfactor)) -height $height
+			mv "$FILE" temp/"$FILE"
+			rm -rf temp
+			find . -maxdepth 1  -name "*.jpg" -exec mv "{}" ./out \;
+			
+		done <batch.txt
+}
+
+FILE=$1
+widthfactor=$2
+batch=$3
+
+
+if [ $batch == 'batchno' ];then
+batchno
+fi
+
+if [ $batch == 'batchyes' ];then
+batchyes
+fi
